6f81a8e0d47a4bbb701a1416bbf25391b8829440
mori_yu <yuuto0609@gmail.com>
2014-01-06 00:00:30 +0900

    ユーザー動作判定を実際に組み込んでみた
    第二ボタンでユーザーターンを切り替え

--- src/com/example/djprototype/PlayFragment.java
+++ src/com/example/djprototype/PlayFragment.java
@@ -2,6 +2,8 @@ package com.example.djprototype;
 
 import java.util.List;
 import com.example.djprototype.MusicPlayer.Mode;
+import com.example.djprototype.UserAction.Move;
+
 import android.content.Context;
 import android.content.res.Resources;
 import android.graphics.drawable.Drawable;
@@ -25,31 +27,32 @@ import android.widget.TextView;
 
 public class PlayFragment extends Fragment implements OnClickListener, SensorEventListener {
 	// Logic
-	MotionHandler			mMotionHandler;
-	MusicPlayer				mMusicPlayer;
+	MotionHandler mMotionHandler;
+	MusicPlayer mMusicPlayer;
+	UserAction mUserAction;
 	// Sensor
-	SensorManager			mSensorManager;
-	List<Sensor>			sensors;
+	SensorManager mSensorManager;
+	List<Sensor> sensors;
 	// View
-	Button					mPlay;
-	Button					mBack;
-	Button					mForward;
-	TextView				mCurrentMode;
-	ImageView				mSound1;
-	ImageView				mSound2;
-	ImageView				mSound3;
+	Button mPlay;
+	Button mBack;
+	Button mForward;
+	TextView mCurrentMode;
+	ImageView mSound1;
+	ImageView mSound2;
+	ImageView mSound3;
 	// Drawable
-	Drawable				play;
-	Drawable				stop;
-	Drawable				rock;
-	Drawable				dj;
-	Drawable				japan;
-	Drawable				debug;
+	Drawable play;
+	Drawable stop;
+	Drawable rock;
+	Drawable dj;
+	Drawable japan;
+	Drawable debug;
 	// Variables
-	ArrayAdapter<String>	adapter;
-	boolean					sensorRun	= true;
-	int						currentTime;
-	Resources				resources;
+	ArrayAdapter<String> adapter;
+	boolean sensorRun = true;
+	int currentTime;
+	Resources resources;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -60,7 +63,14 @@ public class PlayFragment extends Fragment implements OnClickListener, SensorEve
 		mSensorManager = (SensorManager) context.getSystemService(Context.SENSOR_SERVICE);
 		mMusicPlayer = new MusicPlayer(context);
 		mMotionHandler = new MotionHandler();
+		mUserAction = new UserAction();
 		resources = getResources();
+
+		// お試しで正解を作成
+		mUserAction.addCorrectAction(Move.sideSwing);
+		mUserAction.addCorrectAction(Move.sideSwing);
+		mUserAction.addCorrectAction(Move.frontSlide);
+
 		// Viewの設定
 		createView(v);
 		reloadModeView();
@@ -97,49 +107,50 @@ public class PlayFragment extends Fragment implements OnClickListener, SensorEve
 		// TODO Auto-generated method stub
 		int id = v.getId();
 		switch (id) {
-		case R.id.textView_currentMode:
-			mMusicPlayer.changeMode();
-			reloadModeView();
-			break;
-		case R.id.button_play_stop:
-			if (mMusicPlayer.mediaPlayer.isPlaying()) {
-				mMusicPlayer.stopMusic();
-				mPlay.setBackgroundDrawable(play);
-			} else {
-				mMusicPlayer.startMusic();
-				mPlay.setBackgroundDrawable(stop);
-			}
-			break;
-		case R.id.button_back:
-			currentTime = mMusicPlayer.mediaPlayer.getCurrentPosition();
-			mMusicPlayer.mediaPlayer.seekTo(currentTime - 5000);
-			break;
-		case R.id.button_foward:
-			currentTime = mMusicPlayer.mediaPlayer.getCurrentPosition();
-			mMusicPlayer.mediaPlayer.seekTo(currentTime + 5000);
-			break;
-		case R.id.imageView_sound1:
-			if (mMusicPlayer.mCurrentMode == Mode.debug) {
-				FragmentManager fm = getFragmentManager();
-				FragmentTransaction ft = fm.beginTransaction();
-				MainFragment fragment = new MainFragment();
-				ft.replace(R.id.LinearLayout2, fragment);
-				ft.addToBackStack(null);
-				ft.commit();
-			} else {
-				mMusicPlayer.soundRhythmHigh();
-			}
-			break;
-		case R.id.imageView_sound2:
-			// mMusicPlayer.soundRhytmMiddle();
-			mMusicPlayer.startDirection();
-			break;
-		case R.id.imageView_sound3:
-			// mMusicPlayer.soundRhythmLow();
-			mMusicPlayer.stopDirection();
-			break;
-		default:
-			break;
+			case R.id.textView_currentMode:
+				mMusicPlayer.changeMode();
+				reloadModeView();
+				break;
+			case R.id.button_play_stop:
+				if (mMusicPlayer.mediaPlayer.isPlaying()) {
+					mMusicPlayer.stopMusic();
+					mPlay.setBackgroundDrawable(play);
+				} else {
+					mMusicPlayer.startMusic();
+					mPlay.setBackgroundDrawable(stop);
+				}
+				break;
+			case R.id.button_back:
+				currentTime = mMusicPlayer.mediaPlayer.getCurrentPosition();
+				mMusicPlayer.mediaPlayer.seekTo(currentTime - 5000);
+				break;
+			case R.id.button_foward:
+				currentTime = mMusicPlayer.mediaPlayer.getCurrentPosition();
+				mMusicPlayer.mediaPlayer.seekTo(currentTime + 5000);
+				break;
+			case R.id.imageView_sound1:
+				if (mMusicPlayer.mCurrentMode == Mode.debug) {
+					FragmentManager fm = getFragmentManager();
+					FragmentTransaction ft = fm.beginTransaction();
+					MainFragment fragment = new MainFragment();
+					ft.replace(R.id.LinearLayout2, fragment);
+					ft.addToBackStack(null);
+					ft.commit();
+				} else {
+					mMusicPlayer.soundRhythmHigh();
+				}
+				break;
+			case R.id.imageView_sound2:
+				// mMusicPlayer.soundRhytmMiddle();
+				// mMusicPlayer.startDirection();
+				mUserAction.changeUserTurn();
+				break;
+			case R.id.imageView_sound3:
+				// mMusicPlayer.soundRhythmLow();
+				// mMusicPlayer.stopDirection();
+				break;
+			default:
+				break;
 		}
 	}
 
@@ -169,21 +180,22 @@ public class PlayFragment extends Fragment implements OnClickListener, SensorEve
 	}
 
 	private void reloadModeView() {
-		// setBackground()だとAPI 16からなので最小SDKバージョンに対応出来ないので、とりあえずsetBackgroudnDrawable()で対応
+		// setBackground()だとAPI
+		// 16からなので最小SDKバージョンに対応出来ないので、とりあえずsetBackgroudnDrawable()で対応
 		switch (mMusicPlayer.mCurrentMode) {
-		case rock:
-			mCurrentMode.setBackgroundDrawable(rock);
-			break;
-		case dj:
-			mCurrentMode.setBackgroundDrawable(dj);
-			break;
-		case japan:
-			mCurrentMode.setBackgroundDrawable(japan);
-			break;
-		case debug:
-			mCurrentMode.setBackgroundDrawable(debug);
-		default:
-			break;
+			case rock:
+				mCurrentMode.setBackgroundDrawable(rock);
+				break;
+			case dj:
+				mCurrentMode.setBackgroundDrawable(dj);
+				break;
+			case japan:
+				mCurrentMode.setBackgroundDrawable(japan);
+				break;
+			case debug:
+				mCurrentMode.setBackgroundDrawable(debug);
+			default:
+				break;
 		}
 	}
 
@@ -199,16 +211,27 @@ public class PlayFragment extends Fragment implements OnClickListener, SensorEve
 		if (sensorRun) {
 			if (mMotionHandler.frontSlide(event)) {
 				mMusicPlayer.soundMove();
+				mUserAction.addUserAction(Move.frontSlide);
 			}
 			if (mMotionHandler.sideSwing(event)) {
 				mMusicPlayer.soundSwing();
+				mUserAction.addUserAction(Move.sideSwing);
 			}
 			if (mMotionHandler.verticallSlide(event)) {
 				if (mMusicPlayer.mCurrentMode == Mode.dj) {
 					mMusicPlayer.soundSlide();
 				}
+				mUserAction.addUserAction(Move.verticalSlide);
 			}
 			mMotionHandler.reloadData(event);
+
+			if (mUserAction.isFinishedUserAction()) {
+				if (mUserAction.isCorrectUserAction()) {
+					mMusicPlayer.soundCorrect();
+				} else {
+					mMusicPlayer.soundIncorrect();
+				}
+			}
 		}
 	}
 }
